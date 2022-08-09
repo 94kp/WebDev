d5cf3d84abab558d28918b5931ed9c5933d2893f
Karan Patil <67546294+94kp@users.noreply.github.com>
2022-08-09 09:06:29 +0530

    jQuery Section Completed

---
+++ jQuery/index.js
@@ -0,0 +1,9 @@
+
+
+$(document).on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
+
+
+// toggle, hide, show, fadeOut, fadeIn, slideUP, slideDown, slideToggle, animate
+
